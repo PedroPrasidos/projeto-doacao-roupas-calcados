df429b9733099b62b26e31f38ab213355e8245ee
PedroPrasidos <pedroprasidos5@gmail.com>
2026-05-25 16:16:38 -0300

    Adição de campo "tipo" nos detalhes do vestuário, alteração na cor dos atributos na tela de detalhes, onde foi deixado mais escuro. Remoção de header e footer na tela de login.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { FooterComponent } from './components/footer/footer';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  private router = inject(Router);
+  rotaAtual = '';
+
+  constructor() {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe((e: any) => {
+      this.rotaAtual = e.urlAfterRedirects;
+    });
+  }
+
+  estaNoLogin(): boolean {
+    return this.rotaAtual === '/login' || this.rotaAtual === '/usuario/novo';
+  }
+}
